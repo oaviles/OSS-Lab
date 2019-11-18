e6b29b7fb3c863ebe453e1d31c10ca63712a9dcc
Omar <edaviles@live.com>
2019-11-18 14:57:02 -0600

    code updated

--- src/components/plan/Plans.js
+++ src/components/plan/Plans.js
@@ -58,7 +58,7 @@ export default class Plans extends Component {
 
         return (
             <div class="container">
-                <h3>Welcome to Best For You Organics Company : OSS Lab V1.0</h3>
+                <h3>Welcome to Best For You Organics Company : OSS Lab V2.0 : 181119</h3>
                 <Grid>
                     <div class="plans">
                         <Row>{planCatalog}</Row>
