5c18aabd1167c08b1b88056622802e43dc3e8bb1
Omar Aviles <oaviles@live.com>
2021-08-30 17:11:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var userSession = require('./routes/session');
 var app = express();
 
 //var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
-var databaseUrl = 'mongodb://lab-cosmosdbv1:KgdCGb8ehZ7GcVg7OfWONm3Oq2vOWQf0xSyBEJuGLePzj3YoBkI3BqYilABSKX7lYkL7zGwu8vxTa2bUT14LYg==@lab-cosmosdbv1.documents.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb';
+var databaseUrl = 'MONGODB_URI';
 
 
 var mongoose = require('mongoose');
